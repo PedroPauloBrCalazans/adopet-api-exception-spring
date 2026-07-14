f667f9c62075d6f07e02a7f21a5e84cfe8f9491a
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-07-14 00:39:24 -0300

    FEAT: Implementação para isolamento de erros + capturando outras exceções.

--- src/main/java/adopet/api/exception/GlobalExceptionHandler.java
+++ src/main/java/adopet/api/exception/GlobalExceptionHandler.java
@@ -5,11 +5,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AdocaoException.class)
-    public ResponseEntity<String> adocaoException(AdocaoException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<ResponseError> adocaoException(AdocaoException ex) {
+
+        ResponseError response = new ResponseError(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseError> trataException(Exception ex) {
+
+        ResponseError response = new ResponseError(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    } //método coringa para tratar as exceções.
 }
